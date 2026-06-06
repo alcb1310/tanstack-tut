505901a2d5763ed3d3e8af3f485c3f3329e6c63c
Andres Court <andres@andrescourt.com>
2026-06-06 11:34:26 -0500

    added nitro

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    server: {
-        port: 3000,
-    },
-    resolve: {
-        tsconfigPaths: true,
-    },
-    plugins: [
-        tanstackStart(),
-        // teact's vite plugin must come after start's vite plugin
-        viteReact(),
-    ],
+	server: {
+		port: 3000,
+	},
+	resolve: {
+		tsconfigPaths: true,
+	},
+	plugins: [
+		tanstackStart(),
+		nitro(),
+		// teact's vite plugin must come after start's vite plugin
+		viteReact(),
+	],
 });
