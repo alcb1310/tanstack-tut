801e4e3375ffb5a42a8a8b5db1dce097030aaa90
Andres Court <andres@andrescourt.com>
2026-06-06 10:53:59 -0500

    first route

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,11 @@
 import { createRouter } from "@tanstack/react-router";
-import [routeTree] from "./route-tree";
+import { routeTree } from "./route-tree";
 
 export function getRouter() {
-    const router = createRouter({
-        routeTree,
-        scrollRestoration: true
-    });
+	const router = createRouter({
+		routeTree,
+		scrollRestoration: true,
+	});
 
-    return router;
+	return router;
 }
