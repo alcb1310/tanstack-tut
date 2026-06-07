04f7baa5c8896a77a5131ad33e776121ad57d25d
Andres Court <andres@andrescourt.com>
2026-06-06 23:30:39 -0500

    fix get logged in user query

--- src/queries/user.ts
+++ src/queries/user.ts
@@ -3,23 +3,28 @@ import { getCookie } from "@tanstack/react-start/server"
 import type { UserResponse } from "@/types/user"
 
 const URL = import.meta.env.VITE_BACKEND_SERVER
+const cookieName = "BCA-TOKEN"
 
-export const MeQuery = createServerFn({ method: "GET" }).handler(async () => {
-	const token = getCookie("BCA-TOKEN")
-	if (!token) {
-		throw new Error("Usuario no autenticado")
-	}
+export const MeQuery = createServerFn({ method: "GET" }).handler(
+	async (): Promise<UserResponse> => {
+		const token = getCookie(cookieName)
+		if (!token) {
+			throw new Error("Usuario no autenticado")
+		}
 
-	const response = await fetch(`${URL}/me`, {
-		headers: {
-			Authorization: `Bearer ${token}`,
-		},
-	})
+		const response = await fetch(`${URL}/users/me`, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${token}`,
+			},
+		})
 
-	if (!response.ok) {
-		const err = await response.json()
-		throw new Error(err.error)
-	}
+		if (!response.ok) {
+			const data = await response.json()
+			throw new Error(data.error)
+		}
 
-	return (await response.json()) as UserResponse
-})
+		return await response.json()
+	},
+)
