7006fc884ebc1c7a1eb72729dcdfd4da8f589c74
Andres Court <andres@andrescourt.com>
2026-06-07 14:00:54 -0500

    added the rubro material type and schema

--- src/types/rubro-material.ts
+++ src/types/rubro-material.ts
@@ -14,3 +14,14 @@ export const rubroMaterialResponseSchema = z.object({
 export type RubroMaterialResponseTye = z.infer<
 	typeof rubroMaterialResponseSchema
 >
+
+export const rubroMaterialSchema = z.object({
+	item_id: z.string().uuid("Seleccione un rubro"),
+	material_id: z.string().uuid("Seleccione un material"),
+	quantity: z.custom<number>(val => {
+		const num = Number.parseFloat(val as string)
+		return !Number.isNaN(num)
+	}, "La cantidad deber ser un  número"),
+})
+
+export type RubroMaterialType = z.infer<typeof rubroMaterialSchema>
