857ed5732ffa5fef634a781fb190edfe67e05710
Andres Court <andres@andrescourt.com>
2026-06-07 11:46:14 -0500

    added create proveedor type and schema

--- src/types/proveedor.ts
+++ src/types/proveedor.ts
@@ -18,3 +18,16 @@ export const supplierSchema = z.object({
 })
 
 export type SupplierType = z.infer<typeof supplierSchema>
+
+export const supplierCreateSchema = z.object({
+	supplier_id: z
+		.string()
+		.min(1, "Ruc del proveedor es obligatorio")
+		.min(10, "Ruc inválido"),
+	name: z.string().min(1, "Nombre es obligatorio"),
+	contact_name: z.string().optional(),
+	contact_phone: z.string().optional(),
+	contact_email: z.string().optional(),
+})
+
+export type SupplierCreateType = z.infer<typeof supplierCreateSchema>
