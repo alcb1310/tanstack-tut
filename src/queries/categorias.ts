69c7c9301007a5ae1b463f5b2a0fae8dbd6ec90d
Andres Court <andres@andrescourt.com>
2026-06-07 10:10:18 -0500

    added the create category query function

--- src/queries/categorias.ts
+++ src/queries/categorias.ts
@@ -6,7 +6,7 @@ const URL = import.meta.env.VITE_BACKEND_SERVER
 const cookieName = "BCA-TOKEN"
 
 export const GetAllCategories = createServerFn({ method: "GET" }).handler(
-	async () => {
+	async (): Promise<CategoryType[]> => {
 		const token = getCookie(cookieName)
 
 		const response = await fetch(`${URL}/parametros/categorias`, {
@@ -21,8 +21,29 @@ export const GetAllCategories = createServerFn({ method: "GET" }).handler(
 			throw new Error("Network response was not ok")
 		}
 
-		const data = await response.json()
-
-		return data as CategoryType[]
+		return await response.json()
 	},
 )
+
+export const CreateCategory = createServerFn({ method: "POST" })
+	.inputValidator((data: CategoryType) => data)
+	.handler(async ({ data }) => {
+		const token = getCookie(cookieName)
+
+		const response = await fetch(`${URL}/parametros/categorias`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${token}`,
+			},
+			body: JSON.stringify(data),
+		})
+
+		if (!response.ok) {
+			const data = await response.json()
+
+			throw new Error(data.error)
+		}
+
+		return
+	})
