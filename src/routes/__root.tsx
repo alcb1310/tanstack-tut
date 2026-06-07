dc63715dbfe865fc720200dbfc55c2744a21980f
Andres Court <andres@andrescourt.com>
2026-06-06 21:51:33 -0500

    setup shadcn's toaster so sonner can work

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -8,6 +8,7 @@ import {
 import type { ReactNode } from "react"
 import { TooltipProvider } from "@/components/ui/tooltip"
 import appCss from "../styles.css?url"
+import { Toaster } from "@/components/ui/sonner"
 
 export const Route = createRootRouteWithContext<{
 	queryClient: QueryClient
@@ -46,6 +47,7 @@ function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
 			</head>
 			<body>
 				<TooltipProvider>{children}</TooltipProvider>
+				<Toaster />
 				<Scripts />
 			</body>
 		</html>
