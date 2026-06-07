43b76f6df4a8a34bd787c69fe47f77b11947580b
Andres Court <andres@andrescourt.com>
2026-06-07 10:02:52 -0500

    added category type

---
+++ src/types/categorias.ts
@@ -0,0 +1,8 @@
+import { z } from "zod"
+
+export const categorySchema = z.object({
+	id: z.string().uuid().optional(),
+	name: z.string().min(1, "Categoría es obligatoria"),
+})
+
+export type CategoryType = z.infer<typeof categorySchema>
