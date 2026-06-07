74814afc372362a5e57a7938145a21b4fb9a5c1a
Andres Court <andres@andrescourt.com>
2026-06-07 00:11:03 -0500

    user create type and schema

--- src/types/user.ts
+++ src/types/user.ts
@@ -8,3 +8,14 @@ export const userResponseSchema = z.object({
 })
 
 export type UserResponse = z.infer<typeof userResponseSchema>
+
+export const userCreateSchema = z.object({
+	name: z.string().min(1, "Nombre es obligatorio"),
+	email: z.string().min(1, "Email es obligatorio").email("Email no es valido"),
+	password: z
+		.string()
+		.min(1, "Contraseña es obligatoria")
+		.min(8, "Contraseña debe ser mayor a 8 caracteres"),
+})
+
+export type UserCreate = z.infer<typeof userCreateSchema>
