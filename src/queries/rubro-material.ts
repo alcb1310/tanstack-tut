91fb23212bd4fb0256a79c2383d0e53e1d109d6e
Andres Court <andres@andrescourt.com>
2026-06-07 14:01:31 -0500

    added the create rubro material query function

--- src/queries/rubro-material.ts
+++ src/queries/rubro-material.ts
@@ -1,6 +1,9 @@
 import { createServerFn } from "@tanstack/react-start"
 import { getCookie } from "@tanstack/react-start/server"
-import type { RubroMaterialResponseTye } from "@/types/rubro-material"
+import type {
+	RubroMaterialResponseTye,
+	RubroMaterialType,
+} from "@/types/rubro-material"
 
 const URL = import.meta.env.VITE_BACKEND_SERVER
 const cookieName = "BCA-TOKEN"
@@ -30,3 +33,28 @@ export const GetAllRubrosMaterials = createServerFn({ method: "GET" })
 			return response.json()
 		},
 	)
+
+export const CreateRubroMaterial = createServerFn({ method: "POST" })
+	.inputValidator((data: RubroMaterialType) => data)
+	.handler(async ({ data }) => {
+		const token = getCookie(cookieName)
+
+		const response = await fetch(
+			`${URL}/parametros/rubros/${data.item_id}/materiales`,
+			{
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${token}`,
+				},
+				body: JSON.stringify(data),
+			},
+		)
+
+		if (!response.ok) {
+			const error = await response.json()
+			throw new Error(error.error)
+		}
+
+		return
+	})
