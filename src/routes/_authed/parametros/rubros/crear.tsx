de07bece6e0ad48eed2ba8e9173f76a35e46de58
Andres Court <andres@andrescourt.com>
2026-06-07 13:09:45 -0500

    added the crear rubro page

--- src/routes/_authed/parametros/rubros/crear.tsx
+++ src/routes/_authed/parametros/rubros/crear.tsx
@@ -1,9 +1,119 @@
-import { createFileRoute } from "@tanstack/react-router"
+import { FormBackground } from "@/components/layout/form-background"
+import PageTitle from "@/components/layout/page-title"
+import { Button, buttonVariants } from "@/components/ui/button"
+import { FieldGroup, FieldSet } from "@/components/ui/field"
+import { useAppForm } from "@/hooks/app-form"
+import { CreateRubro } from "@/queries/rubros"
+import { rubrosSchema, type RubrosType } from "@/types/rubros"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { createFileRoute, Link, useNavigate } from "@tanstack/react-router"
+import { SaveIcon, CircleXIcon } from "lucide-react"
+import { toast } from "sonner"
 
 export const Route = createFileRoute("/_authed/parametros/rubros/crear")({
 	component: RouteComponent,
 })
 
 function RouteComponent() {
-	return <div>Hello "/_authed/parametros/rubros/crear"!</div>
+	const queryClient = useQueryClient()
+	const navigate = useNavigate()
+
+	const createRubroMutation = useMutation({
+		mutationFn: CreateRubro,
+		onSuccess: data => {
+			toast.success("Rubro creado exitosamente")
+			queryClient.invalidateQueries({ queryKey: ["rubros"] })
+			navigate({
+				to: "/parametros/rubros/$rubroId",
+				params: { rubroId: data.id as string },
+			})
+		},
+		onError: error => {
+			toast.error(error.message, {
+				position: "top-center",
+				style: {
+					color: "red",
+				},
+			})
+		},
+	})
+
+	const form = useAppForm({
+		defaultValues: {
+			code: "",
+			name: "",
+			unit: "",
+		} as RubrosType,
+		validators: {
+			onSubmit: rubrosSchema,
+		},
+		onSubmit: data => {
+			createRubroMutation.mutate({ data: data.value })
+		},
+	})
+
+	return (
+		<div>
+			<PageTitle title='Crear Rubro' />
+
+			<FormBackground>
+				<form
+					onSubmit={e => {
+						e.preventDefault()
+						e.stopPropagation()
+						form.handleSubmit()
+					}}
+				>
+					<FieldGroup className='my-2 px-4'>
+						<FieldSet>
+							<form.AppField name='code'>
+								{field => (
+									<field.TextField
+										name='code'
+										label='Codigo'
+										placeholder='Codigo'
+									/>
+								)}
+							</form.AppField>
+
+							<form.AppField name='name'>
+								{field => (
+									<field.TextField
+										name='name'
+										label='Nombre'
+										placeholder='Nombre'
+									/>
+								)}
+							</form.AppField>
+
+							<form.AppField name='unit'>
+								{field => (
+									<field.TextField
+										name='unit'
+										label='Unidad'
+										placeholder='Unidad'
+									/>
+								)}
+							</form.AppField>
+						</FieldSet>
+					</FieldGroup>
+
+					<div className='flex justify-around items-center '>
+						<Button type='submit'>
+							<SaveIcon size={10} />
+							Guardar
+						</Button>
+
+						<Link
+							to='/parametros/rubros'
+							className={buttonVariants({ variant: "secondary" })}
+						>
+							<CircleXIcon size={10} />
+							Cancelar
+						</Link>
+					</div>
+				</form>
+			</FormBackground>
+		</div>
+	)
 }
