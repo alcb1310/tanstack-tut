26e042a6219e91d193a987cd1912f0eea7d691a6
Andres Court <andres@andrescourt.com>
2026-06-08 17:39:21 -0500

    form background layout mobile ready

--- src/components/layout/form-background.tsx
+++ src/components/layout/form-background.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from "react"
 
 export function FormBackground({ children }: { children: ReactNode }) {
 	return (
-		<div className='w-1/2 mx-auto my-3 p-3 bg-sidebar-primary-foreground '>
+		<div className='md:w-1/2 md:mx-auto my-3 p-3 bg-sidebar-primary-foreground '>
 			{children}
 		</div>
 	)
