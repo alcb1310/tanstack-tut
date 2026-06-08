9224d853d232be93447c5081d90ec0d3f71a13b0
Andres Court <andres@andrescourt.com>
2026-06-07 22:01:23 -0500

    added the detalles create type and schema

--- src/types/detalle.ts
+++ src/types/detalle.ts
@@ -15,3 +15,23 @@ export const invoiceDetailsResponseSchema = z.object({
 export type InvoiceDetailsResponseType = z.infer<
 	typeof invoiceDetailsResponseSchema
 >
+
+export const invoiceDetailsCreateSchema = z.object({
+	budget_item_id: z.string().uuid("Seleccione una partida"),
+	quantity: z.custom<number>(val => {
+		const num = Number.parseFloat(val as string)
+		return !Number.isNaN(num) || val === ""
+	}, "La cantidad  debe ser un número"),
+	cost: z.custom<number>(val => {
+		const num = Number.parseFloat(val as string)
+		return !Number.isNaN(num) || val === ""
+	}, "El costo debe ser un número"),
+	total: z.custom<number>(val => {
+		const num = Number.parseFloat(val as string)
+		return !Number.isNaN(num) || val === ""
+	}, "El total debe ser un número"),
+})
+
+export type InvoiceDetailsCreateType = z.infer<
+	typeof invoiceDetailsCreateSchema
+>
