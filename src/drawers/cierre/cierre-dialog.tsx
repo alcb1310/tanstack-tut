7e1fb5aa48d7a158dba86dceb2a8ce890d5c40fa
Andres Court <andres@andrescourt.com>
2026-06-08 17:38:23 -0500

    cierre dialog

---
+++ src/drawers/cierre/cierre-dialog.tsx
@@ -0,0 +1,86 @@
+import { useMutation, useQuery } from "@tanstack/react-query"
+import { InfoIcon, SaveIcon } from "lucide-react"
+import { toast } from "sonner"
+import {
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogMedia,
+	AlertDialogTitle,
+	AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { Button } from "@/components/ui/button"
+import { CreateClosure } from "@/queries/cierre-mensual"
+import { GetOneProject } from "@/queries/proyectos"
+
+type CierreDialogProps = {
+	projectId: string
+	date: string
+}
+
+export function CierreDialog({ projectId, date }: CierreDialogProps) {
+	const { data } = useQuery({
+		queryKey: ["proyectos", projectId],
+		queryFn: () => GetOneProject({ data: { id: projectId } }),
+	})
+
+	const useGenerateClosureMutation = useMutation({
+		mutationFn: CreateClosure,
+		onSuccess: () => {
+			toast.success("Cierre creado exitosamente")
+		},
+		onError: error => {
+			toast.error(error.message, {
+				position: "top-center",
+				style: {
+					color: "red",
+				},
+			})
+		},
+	})
+
+	return (
+		<AlertDialog>
+			<AlertDialogTrigger asChild>
+				<Button type='submit'>
+					<SaveIcon size={10} />
+					Generar Cierre
+				</Button>
+			</AlertDialogTrigger>
+			<AlertDialogContent>
+				<AlertDialogHeader>
+					<AlertDialogMedia className='bg-white'>
+						<InfoIcon size={16} className='bg-white text-primary' />
+					</AlertDialogMedia>
+					<AlertDialogTitle className='text-text-primary'>
+						Generar Cierre
+					</AlertDialogTitle>
+					<AlertDialogDescription>
+						¿Desea generar el cierre del proyecto{" "}
+						<span className='font-bold'>{data?.name}</span> para la fecha&nbsp;
+						<span className='font-bold'>{date}</span>?
+					</AlertDialogDescription>
+				</AlertDialogHeader>
+				<AlertDialogFooter>
+					<AlertDialogCancel>Cancelar</AlertDialogCancel>
+					<AlertDialogAction
+						onClick={() =>
+							useGenerateClosureMutation.mutate({
+								data: {
+									project_id: projectId,
+									date: new Date(date),
+								},
+							})
+						}
+					>
+						Generar
+					</AlertDialogAction>
+				</AlertDialogFooter>
+			</AlertDialogContent>
+		</AlertDialog>
+	)
+}
