6ce140d96c259d952ce185fe2e4c7ac7015843aa
Andres Court <andres@andrescourt.com>
2026-06-08 17:35:52 -0500

    added cierre type and schema

---
+++ src/types/cierre.ts
@@ -0,0 +1,10 @@
+import { z } from "zod"
+
+export const cierreSchema = z.object({
+	project_id: z.string().uuid("Seleccione un proyecto"),
+	date: z.coerce.date({
+		message: "Ingrese una fecha",
+	}),
+})
+
+export type CierreTypes = z.infer<typeof cierreSchema>
