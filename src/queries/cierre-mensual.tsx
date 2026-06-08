95872196289b38be96f7966dee563c3ffe507dbd
Andres Court <andres@andrescourt.com>
2026-06-08 17:37:24 -0500

    added create cierre query function

---
+++ src/queries/cierre-mensual.tsx
@@ -0,0 +1,28 @@
+import { createServerFn } from "@tanstack/react-start"
+import { getCookie } from "@tanstack/react-start/server"
+import type { CierreTypes } from "@/types/cierre"
+
+const URL = import.meta.env.VITE_BACKEND_SERVER
+const cookieName = "BCA-TOKEN"
+
+export const CreateClosure = createServerFn({ method: "POST" })
+	.inputValidator((data: CierreTypes) => data)
+	.handler(async ({ data }) => {
+		const token = getCookie(cookieName)
+
+		const response = await fetch(`${URL}/transacciones/cierre`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${token}`,
+			},
+			body: JSON.stringify(data),
+		})
+
+		if (!response.ok) {
+			const error = await response.json()
+			throw new Error(error.error)
+		}
+
+		return
+	})
