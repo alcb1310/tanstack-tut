4ebcc8ab25787773dfd735e9be416669e9739646
Andres Court <andres@andrescourt.com>
2026-06-09 16:31:54 -0500

    added the get analisis query function

---
+++ src/queries/analisis.ts
@@ -0,0 +1,26 @@
+import { createServerFn } from "@tanstack/react-start"
+import { getCookie } from "@tanstack/react-start/server"
+import type { AnalisisType } from "@/types/analisis"
+
+const URL = import.meta.env.VITE_BACKEND_SERVER
+const cookieName = "BCA-TOKEN"
+
+export const GetAnalisis = createServerFn({ method: "GET" })
+	.inputValidator((data: { id: string }) => data)
+	.handler(async ({ data }): Promise<AnalisisType[]> => {
+		const token = getCookie(cookieName)
+
+		const response = await fetch(`${URL}/analisis/${data.id}`, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${token}`,
+			},
+		})
+
+		if (!response.ok) {
+			throw new Error("Network response was not ok")
+		}
+
+		return await response.json()
+	})
