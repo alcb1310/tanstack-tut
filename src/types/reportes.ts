66cb2614de62afa89d0e61d7de84eac18a61a4f5
Andres Court <andres@andrescourt.com>
2026-06-09 17:14:51 -0500

    added the actual report type and schema

--- src/types/reportes.ts
+++ src/types/reportes.ts
@@ -1,4 +1,17 @@
+import { z } from "zod"
+
 export type LevelType = {
 	key: string
 	value: string
 }
+
+export const actualReportSchema = z.object({
+	project_id: z
+		.string({ message: "Seleccione un proyecto" })
+		.uuid("Seleccione un proyecto"),
+	level: z
+		.string({ message: "Seleccione un nivel" })
+		.min(1, "Seleccione un nivel"),
+})
+
+export type ActualReportTypes = z.infer<typeof actualReportSchema>
