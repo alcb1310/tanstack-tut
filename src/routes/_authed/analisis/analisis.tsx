44a14ebe1321c4a4797ade860ccee16803083cd4
Andres Court <andres@andrescourt.com>
2026-06-09 16:33:56 -0500

    analisis report page

--- src/routes/_authed/analisis/analisis.tsx
+++ src/routes/_authed/analisis/analisis.tsx
@@ -1,9 +1,100 @@
+import { useQuery, useSuspenseQuery } from "@tanstack/react-query"
 import { createFileRoute } from "@tanstack/react-router"
+import { PlayIcon } from "lucide-react"
+import { useState } from "react"
+import { toast } from "sonner"
+import { AnalisisCollapsible } from "@/collapsibles/analisis"
+import { FormBackground } from "@/components/layout/form-background"
+import PageTitle from "@/components/layout/page-title"
+import { Button } from "@/components/ui/button"
+import { NativeSelect, NativeSelectOption } from "@/components/ui/native-select"
+import { Spinner } from "@/components/ui/spinner"
+import { GetAnalisis } from "@/queries/analisis"
+import { GetAllProjects } from "@/queries/proyectos"
 
 export const Route = createFileRoute("/_authed/analisis/analisis")({
 	component: RouteComponent,
+	loader: ({ context: { queryClient } }) => {
+		queryClient.prefetchQuery({
+			queryKey: ["proyectos"],
+			queryFn: () => GetAllProjects({ data: { active: true } }),
+		})
+	},
+	shouldReload: true,
+	gcTime: 0,
+	preloadStaleTime: 0,
 })
 
 function RouteComponent() {
-	return <div>Hello "/_authed/analisis/analisis"!</div>
+	const [project, setProject] = useState<string>("")
+	const { data: projects } = useSuspenseQuery({
+		queryKey: ["proyectos"],
+		queryFn: () => GetAllProjects({ data: { active: true } }),
+	})
+
+	const { data, isLoading, isFetching, refetch } = useQuery({
+		queryKey: ["analisis"],
+		queryFn: () => GetAnalisis({ data: { id: project } }),
+		enabled: false,
+	})
+
+	const proyValues =
+		projects?.map(item => ({
+			label: item.name,
+			value: item.id as string,
+		})) || []
+	proyValues.unshift({
+		label: "Seleccione un proyecto",
+		value: "",
+	})
+
+	const show = data?.map(item => (
+		<AnalisisCollapsible key={item.key} data={item} />
+	))
+
+	return (
+		<div>
+			<PageTitle title='Analisis' />
+			<FormBackground>
+				<NativeSelect
+					name={"proyectos"}
+					className='w-full'
+					size='default'
+					value={project}
+					onChange={e => {
+						setProject(e.target.value)
+					}}
+				>
+					{proyValues.map(option => (
+						<NativeSelectOption key={option.value} value={option.value}>
+							{option.label}
+						</NativeSelectOption>
+					))}
+				</NativeSelect>
+				<div className='my-4 flex justify-start gap-4'>
+					<Button
+						onClick={e => {
+							e.preventDefault()
+							e.stopPropagation()
+
+							if (!project) {
+								toast.error("Seleccione un proyecto", {
+									position: "top-center",
+									style: {
+										color: "red",
+									},
+								})
+							}
+							refetch()
+						}}
+					>
+						<PlayIcon size={16} />
+						Generar
+					</Button>
+				</div>
+			</FormBackground>
+			{(isLoading || isFetching) && <Spinner />}
+			{show}
+		</div>
+	)
 }
