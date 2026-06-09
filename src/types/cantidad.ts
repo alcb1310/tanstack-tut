3871b0917c140415feaa447b2d81f4b313f743bf
Andres Court <andres@andrescourt.com>
2026-06-08 21:31:13 -0500

    added the cantidad edit type and schema

--- src/types/cantidad.ts
+++ src/types/cantidad.ts
@@ -22,3 +22,15 @@ export const quantityCreateSchema = z.object({
 })
 
 export type QuantityCreateType = z.infer<typeof quantityCreateSchema>
+
+export const quantityEditSchema = z.object({
+	id: z.string().uuid(),
+	project_id: z.string().uuid({ message: "Seleccione un proyecto" }),
+	rubro_id: z.string().uuid({ message: "Seleccione un rubro" }),
+	quantity: z.custom<number>(val => {
+		const num = Number.parseFloat(val as string)
+		return !Number.isNaN(num) || val === ""
+	}, "La cantidad  debe ser un número"),
+})
+
+export type QuantityEditType = z.infer<typeof quantityEditSchema>
