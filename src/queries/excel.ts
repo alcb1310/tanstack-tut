b64d08b5271c94b17b5955b4d6ea5e08b0c2c3ce
Andres Court <andres@andrescourt.com>
2026-06-09 17:47:58 -0500

    added the hisroic excel export query function

--- src/queries/excel.ts
+++ src/queries/excel.ts
@@ -1,6 +1,10 @@
 import { createServerFn } from "@tanstack/react-start"
 import { getCookie } from "@tanstack/react-start/server"
-import type { ActualReportTypes, BalanceReportType } from "@/types/reportes"
+import type {
+	ActualReportTypes,
+	BalanceReportType,
+	ReportTypes,
+} from "@/types/reportes"
 
 const URL = import.meta.env.VITE_BACKEND_SERVER
 
@@ -43,6 +47,34 @@ export const balanceExcelExport = createServerFn({ method: "GET" })
 
 		const res = await fetch(
 			`${URL}/reportes/excel/cuadre?project=${data.project_id}&date=${date}`,
+			{
+				method: "GET",
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			},
+		)
+
+		const blob = await res.blob()
+
+		return new Response(blob, {
+			status: 200,
+			headers: {
+				"Content-Type":
+					"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+				"Content-Disposition": 'attachment; filename="reporte.xlsx"',
+			},
+		})
+	})
+
+export const histroricExcelExport = createServerFn({ method: "GET" })
+	.inputValidator((data: ReportTypes) => data)
+	.handler(async ({ data }) => {
+		const token = getCookie("BCA-TOKEN")
+		const date = new Date(data.date).toISOString()
+
+		const res = await fetch(
+			`${URL}/reportes/excel/historico?proyecto=${data.project_id}&nivel=${data.level}&fecha=${date}`,
 			{
 				method: "GET",
 				headers: {
