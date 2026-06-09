781d2908a4e6d14d368f946cbf2359fc1be39837
Andres Court <andres@andrescourt.com>
2026-06-09 16:31:30 -0500

    added the analisis data type and schema

---
+++ src/types/analisis.ts
@@ -0,0 +1,18 @@
+import { z } from "zod"
+
+const analisysDataSchema = z.object({
+	project_name: z.string(),
+	category_name: z.string(),
+	material_name: z.string(),
+	unit: z.string(),
+	quantity: z.number(),
+})
+
+export type AnalisysDataType = z.infer<typeof analisysDataSchema>
+
+const analisysSchema = z.object({
+	key: z.string(),
+	data: z.array(analisysDataSchema),
+})
+
+export type AnalisisType = z.infer<typeof analisysSchema>
