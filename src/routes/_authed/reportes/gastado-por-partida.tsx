4b8ba8d0fc36bf08229f1b505423dd0d236f420e
Andres Court <andres@andrescourt.com>
2026-06-09 20:45:48 -0500

    gastado por partida page

--- src/routes/_authed/reportes/gastado-por-partida.tsx
+++ src/routes/_authed/reportes/gastado-por-partida.tsx
@@ -1,9 +1,219 @@
+import { FormBackground } from "@/components/layout/form-background"
+import PageTitle from "@/components/layout/page-title"
+import { ReportDataTable } from "@/components/table/report-data-table"
+import { Button } from "@/components/ui/button"
+import { FieldGroup, FieldSet } from "@/components/ui/field"
+import { Spinner } from "@/components/ui/spinner"
+import { SpentDetailsDrawer } from "@/drawers/reportes/gastado-por-partida"
+import { useAppForm } from "@/hooks/app-form"
+import { downloadExcelFile } from "@/lib/excel-download"
+import { spentExcelExport } from "@/queries/excel"
+import { GetAllProjects } from "@/queries/proyectos"
+import { GetAllLevels, GetSpentReport } from "@/queries/reportes"
+import { reportSchema, type Spent, type ReportTypes } from "@/types/reportes"
+import { useQuery, useSuspenseQueries } from "@tanstack/react-query"
 import { createFileRoute } from "@tanstack/react-router"
+import type { ColumnDef } from "@tanstack/react-table"
+import { PlayIcon, DownloadIcon } from "lucide-react"
 
 export const Route = createFileRoute("/_authed/reportes/gastado-por-partida")({
 	component: RouteComponent,
+	loader: async ({ context: { queryClient } }) => {
+		Promise.all([
+			queryClient.ensureQueryData({
+				queryKey: ["proyectos", "active"],
+				queryFn: () => GetAllProjects({ data: { active: true } }),
+			}),
+			queryClient.ensureQueryData({
+				queryKey: ["niveles"],
+				queryFn: () => GetAllLevels(),
+			}),
+		])
+	},
 })
 
 function RouteComponent() {
-	return <div>Hello "/_authed/reportes/gastado-por-partida"!</div>
+	const form = useAppForm({
+		defaultValues: {
+			project_id: "",
+			level: "",
+			date: "",
+		} as ReportTypes,
+		validators: {
+			onSubmit: reportSchema,
+		},
+		onSubmit: () => {
+			refetch()
+		},
+	})
+
+	const { data, isLoading, isFetching, refetch } = useQuery({
+		queryKey: ["historico", form.state.values],
+		queryFn: () => GetSpentReport({ data: form.state.values }),
+		enabled:
+			form.state.values.project_id !== "" &&
+			form.state.values.level !== "" &&
+			form.state.values.date !== "",
+	})
+
+	const columns: ColumnDef<Spent>[] = [
+		{
+			accessorKey: "budget_item.code",
+			header: "Código",
+		},
+		{
+			accessorKey: "budget_item.name",
+			header: "Partida",
+		},
+		{
+			accessorKey: "spent",
+			header: "Total",
+			cell: ({ row }) => {
+				return (
+					<span className='block w-full text-right'>
+						{row.original.spent.toLocaleString("es-EC", {
+							minimumFractionDigits: 2,
+							maximumFractionDigits: 2,
+						})}
+					</span>
+				)
+			},
+		},
+		{
+			id: "actions",
+			cell: ({ row }) => {
+				return (
+					<SpentDetailsDrawer
+						report={row.original}
+						project_id={form.state.values.project_id}
+						date={form.state.values.date}
+					/>
+				)
+			},
+		},
+	]
+
+	const results = useSuspenseQueries({
+		queries: [
+			{
+				queryKey: ["proyectos", "active"],
+				queryFn: () => GetAllProjects({ data: { active: true } }),
+			},
+			{
+				queryKey: ["niveles"],
+				queryFn: () => GetAllLevels(),
+			},
+		],
+	})
+
+	const proyValues =
+		results[0].data?.map(item => ({
+			label: item.name,
+			value: item.id as string,
+		})) || []
+	proyValues.unshift({
+		label: "Seleccione un proyecto",
+		value: "",
+	})
+
+	const levelValues = results[1].data?.map(item => ({
+		label: item.value,
+		value: item.key,
+	}))
+	levelValues.unshift({
+		label: "Seleccione un nivel",
+		value: "",
+	})
+	return (
+		<div>
+			<PageTitle title='Gastado por partida' />
+
+			<FormBackground>
+				<form
+					onSubmit={e => {
+						e.preventDefault()
+						e.stopPropagation()
+						form.handleSubmit()
+					}}
+				>
+					<FieldGroup>
+						<FieldSet>
+							<form.AppField name='project_id'>
+								{field => (
+									<field.SelectField
+										label='Proyecto'
+										name='project_id'
+										options={proyValues}
+									/>
+								)}
+							</form.AppField>
+
+							<div className='flex justify-between items-end'>
+								<div className='w-4/12'>
+									<form.AppField name='level'>
+										{field => (
+											<field.SelectField
+												label='Nivel'
+												name='level'
+												options={levelValues}
+											/>
+										)}
+									</form.AppField>
+								</div>
+
+								<div className='w-6/12'>
+									<form.AppField name='date'>
+										{field => (
+											<field.TextField label='Fecha' name='date' type='date' />
+										)}
+									</form.AppField>
+								</div>
+							</div>
+						</FieldSet>
+						<div className='my-4 flex justify-start gap-4'>
+							<Button type='submit'>
+								<PlayIcon size={16} />
+								Generar
+							</Button>
+
+							<Button
+								type='button'
+								variant='detail'
+								onClick={async e => {
+									e.preventDefault()
+									e.stopPropagation()
+
+									if (!form.state.values.project_id || !form.state.values.level)
+										return
+
+									try {
+										const b = await spentExcelExport({
+											data: form.state.values,
+										})
+
+										downloadExcelFile(
+											await b.blob(),
+											"gastado=por=partida.xlsx",
+										)
+									} catch (e) {
+										console.error(e)
+									}
+								}}
+							>
+								<DownloadIcon size={16} />
+								Exportar
+							</Button>
+						</div>
+					</FieldGroup>
+				</form>
+			</FormBackground>
+			{(isLoading || isFetching) && <Spinner />}
+
+			{data && data.spent.length > 0 && (
+				<div className='w-2/3 mx-auto'>
+					<ReportDataTable data={data.spent} columns={columns} />
+				</div>
+			)}
+		</div>
+	)
 }
