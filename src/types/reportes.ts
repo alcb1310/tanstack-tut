89db2b8320eff26a044622e8bfc5a7309a07c7da
Andres Court <andres@andrescourt.com>
2026-06-09 20:43:09 -0500

    added spent details type

--- src/types/reportes.ts
+++ src/types/reportes.ts
@@ -53,3 +53,23 @@ export type SpentResponseType = {
 	total: number
 	project: string
 }
+
+export type SpentDetailsType = {
+	budget_item_code: string
+	budget_item_id: string
+	budget_item_name: string
+	budget_item_level: number
+	company_id: string
+	cost: number
+	invoice_date: string
+	invoice_id: string
+	invoice_number: string
+	invoice_total: number
+	project_id: string
+	project_name: string
+	quantity: number
+	supplier_id: string
+	supplier_name: string
+	supplier_number: string
+	total: number
+}
