b836eac2a97065c6435e1b4492ef618afd78e918
Andres Court <andres@andrescourt.com>
2026-07-30 17:15:59 -0500

    refactor(get analisis): use error response type and validator

--- src/queries/analisis.ts
+++ src/queries/analisis.ts
@@ -1,12 +1,13 @@
 import { createServerFn } from "@tanstack/react-start"
 import { getCookie } from "@tanstack/react-start/server"
 import type { AnalisisType } from "@/types/analisis"
+import type { ErrorResponseType } from "@/types/error"
 
 const URL = import.meta.env.VITE_BACKEND_SERVER
 const cookieName = "BCA-TOKEN"
 
 export const GetAnalisis = createServerFn({ method: "GET" })
-	.inputValidator((data: { id: string }) => data)
+	.validator((data: { id: string }) => data)
 	.handler(async ({ data }): Promise<AnalisisType[]> => {
 		const token = getCookie(cookieName)
 
@@ -19,7 +20,8 @@ export const GetAnalisis = createServerFn({ method: "GET" })
 		})
 
 		if (!response.ok) {
-			throw new Error("Network response was not ok")
+			const res = (await response.json()) as ErrorResponseType
+			throw new Error(res.msg)
 		}
 
 		return await response.json()
